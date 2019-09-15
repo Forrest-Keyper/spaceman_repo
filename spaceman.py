fcb291686b3561cc820569c8e8426aa743606820
Parker M Paisley <ppaisley94@gmail.com>
2019-09-15 13:44:08 -0700

    Function launch_word_load now draws a word, saves it to a variable as string and another as list. Game state inits as gameLoad at bottom of game. Next step will be implementing blanks and user input.

--- spaceman.py
+++ spaceman.py
@@ -1,8 +1,10 @@
 import random
 
-launch_word = []
+launch_word = None
+launch_word_list = None
 
-def space_word_load():
+def launch_word_load():
+    # we'll use this one function to load, split and save our word as a whole word and a list
 
     f = open('word_bank.py', 'r')
 
@@ -11,16 +13,26 @@ def space_word_load():
 
     word_generate = words_list[0].split(' ')
     space_word = random.choice(word_generate)
-    launch_word = list(space_word)
 
-    return space_word
+    global launch_word
+    launch_word = space_word
+
+    global launch_word_list
+    launch_word_list = space_word.split(',')
+
+    return launch_word
+    return launch_word_list
+
+def space_word_check(space_word):
+    print (space_word)
 
 class spaceGame:
 
     #we'll use classes to make it easer to store our chose words and test number of letters
-    def __init__(self, launch_word):
+    def __init__(self, launch_word, launch_word_list):
         self.name = self
         self.launch_word = launch_word
+        self.launch_word_list = launch_word_list
 
 
 
@@ -32,10 +44,16 @@ class spaceGame:
 
 
 def test():
-    print(space_word_load())
-    print("our launch_word is " + launch_word)
+    print("our launch_word is " + game.launch_word)
 
 
+
+def gameLoad():
+    load = launch_word_load()
+    print("Welcome to Spaceman. Try to guess the correct letters to launch the shuttle!")
+
+
+gameLoad()
+
+game = spaceGame(launch_word, launch_word_list)
 test()
-
-game = spaceGame(launch_word)
